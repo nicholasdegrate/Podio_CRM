948be34389d51c241013cfb4c50b6ecda10d4344
nicholasdegrate <nicholasdegrate@gmail.com>
2021-02-11 11:59:36 -0600

    init

---
+++ lib/Podio.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Podio/version"
+
+module Podio
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Podio/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Podio
+  VERSION = "0.1.0"
+end
